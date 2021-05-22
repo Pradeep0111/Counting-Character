5154a4ae058f7d99cb9af49ac4a9980c61caeae2
Pradeep0111 <84283114+Pradeep0111@users.noreply.github.com>
2021-05-23 00:57:56 +0530

    Create count_Char.py

---
+++ count_Char.py
@@ -0,0 +1,11 @@
+def count_char(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+n=input("Enter the character: ")
+print(count_char(n))
